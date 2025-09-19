a87ae76af970915967b2705a3c33a219c76ab992
Kavindu Bandara <it24101943@my.sliit.lk>
2025-09-19 22:20:14 +0530

    Initial

--- blood-donation/src/main/java/com/blooddonation/BloodDonationSystemApplication.java
+++ blood-donation/src/main/java/com/blooddonation/BloodDonationSystemApplication.java
@@ -8,6 +8,7 @@ public class BloodDonationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloodDonationSystemApplication.class, args);
+        System.out.println("Blood Donation System Application has been started");
 	}
 
 }
